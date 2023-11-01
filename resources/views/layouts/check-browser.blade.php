10e0b351c2acb83a01534915d2c27d1e264955f0
Konrem <lord.kazakovv@gmail.com>
2023-11-01 16:59:48 +0200

    version: php 8.0 and Latavel 9

--- resources/views/layouts/check-browser.blade.php
+++ resources/views/layouts/check-browser.blade.php
@@ -1,47 +1,48 @@
 @php
 $browser = new Browser();
+$browserVersion = (float)explode('.', $browser->getVersion())[0];
 @endphp
 
-@if( $browser->getBrowser() == Browser::BROWSER_CHROME && $browser->getVersion() <= 56  )
+@if( $browser->getBrowser() == Browser::BROWSER_CHROME && $browserVersion <= 56  )
     @component('alert')
         Для коректної роботи сервісу оновіть ваш браузер
     @endcomponent
 @endif
 
-@if( $browser->getBrowser() == Browser::BROWSER_FIREFOX && $browser->getVersion() <= 51 )
+@if( $browser->getBrowser() == Browser::BROWSER_FIREFOX && $browserVersion <= 51 )
     @component('alert')
         Для коректної роботи сервісу оновіть ваш браузер
     @endcomponent
 @endif
 
-@if( $browser->getBrowser() == Browser::BROWSER_OPERA && $browser->getVersion() <= 43 )
-    @component('alert')
-        Для коректної роботи сервісу оновіть ваш браузер
-    @endcomponent
-@endif
-
-
-@if( $browser->getBrowser() == Browser::BROWSER_IE && $browser->getVersion() <= 11 )
-    @component('alert')
-        Для коректної роботи сервісу оновіть ваш браузер
-    @endcomponent
-@endif
-
-@if( $browser->getBrowser() == Browser::BROWSER_EDGE && $browser->getVersion() <= 15 )
+@if( $browser->getBrowser() == Browser::BROWSER_OPERA && $browserVersion <= 43 )
     @component('alert')
         Для коректної роботи сервісу оновіть ваш браузер
     @endcomponent
 @endif
 
 
-@if( $browser->getBrowser() == Browser::BROWSER_SAFARI && $browser->getVersion() <= 10 )
+@if( $browser->getBrowser() == Browser::BROWSER_IE && $browserVersion <= 11 )
+    @component('alert')
+        Для коректної роботи сервісу оновіть ваш браузер
+    @endcomponent
+@endif
+
+@if( $browser->getBrowser() == Browser::BROWSER_EDGE && $browserVersion <= 15 )
     @component('alert')
         Для коректної роботи сервісу оновіть ваш браузер
     @endcomponent
 @endif
 
 
-@if( $browser->getBrowser() == Browser::BROWSER_IPHONE && $browser->getVersion() <= 10.2 )
+@if( $browser->getBrowser() == Browser::BROWSER_SAFARI && $browserVersion <= 10 )
+    @component('alert')
+        Для коректної роботи сервісу оновіть ваш браузер
+    @endcomponent
+@endif
+
+
+@if( $browser->getBrowser() == Browser::BROWSER_IPHONE && $browserVersion <= 10.2 )
     @component('alert')
         Для коректної роботи сервісу оновіть ваш браузер
     @endcomponent
